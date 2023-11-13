57685122691d10b11afca0fd1b6e50b020e5c5e5
Manwë <36736607+ManweMSU@users.noreply.github.com>
2023-11-13 11:49:48 +0300

    Добавлен сценарий очистки для Mac OS.

---
+++ purify.sh
@@ -0,0 +1,11 @@
+cd /Users/manwe/Documents/GitHub/EngineJIT
+rm -rf xcon/_build
+rm -rf xv_mm/_build
+rm -rf xv_sl/_build
+rm -rf xx_xx/_build
+rm -rf xx_xxf/_build
+rm -rf xv_com/_build
+rm -rf xi_tool/_build
+rm -rf xx_xxsc/_build
+rm -rf xv_release/_build
+rm -rf xx_release/_build
