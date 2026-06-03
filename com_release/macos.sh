6116393b561aea00e6c03b4931c641b1ad5e2ec0
Demetrius Æmilianus <36736607+ManweMSU@users.noreply.github.com>
2026-06-03 23:36:04 +0300

    Beta 8.

--- com_release/macos.sh
+++ com_release/macos.sh
@@ -9,11 +9,11 @@ xv com_release/fid.xv -Ndr 'xv_release/_build/macosx_x64/XV Monstrans Manualis.a
     'xx_release/_build/macosx_arm64/XX.app/xxcl'\
     'xx_release/_build/macosx_arm64/XX.app/xxi'
 xv com_release/arc.xv -Ndr 'xv_release/_build/macosx_x64/XV Monstrans Manualis.app'\
-    com_release/manifesta/xv_mac.manifest com_release/xv_10_b7_mac_x64.ecsa
+    com_release/manifesta/xv_mac.manifest com_release/xv_10_b8_mac_x64.ecsa
 xv com_release/arc.xv -Ndr 'xv_release/_build/macosx_arm64/XV Monstrans Manualis.app'\
-    com_release/manifesta/xv_mac.manifest com_release/xv_10_b7_mac_arm64.ecsa
+    com_release/manifesta/xv_mac.manifest com_release/xv_10_b8_mac_arm64.ecsa
 xv com_release/arc.xv -Ndr 'xx_release/_build/macosx_x64/XX.app'\
-    com_release/manifesta/xx_mac.manifest com_release/xx_10_b7_mac_x64.ecsa
+    com_release/manifesta/xx_mac.manifest com_release/xx_10_b8_mac_x64.ecsa
 xv com_release/arc.xv -Ndr 'xx_release/_build/macosx_arm64/XX.app'\
-    com_release/manifesta/xx_mac.manifest com_release/xx_10_b7_mac_arm64.ecsa
+    com_release/manifesta/xx_mac.manifest com_release/xx_10_b8_mac_arm64.ecsa
 ./purify.sh
--- com_release/windows.bat
+++ com_release/windows.bat
@@ -13,15 +13,15 @@ xv com_release/fid.xv -Ndr "xv_release/_build/windows_x86/xi"^
     "xx_release/_build/windows_arm64/xxcl"^
     "xx_release/_build/windows_arm64/xxi"
 xv com_release/arc.xv -Ndr "xv_release/_build/windows_x86"^
-    com_release/manifesta/xv_win.manifest com_release/xv_10_b7_win_x86.ecsa
+    com_release/manifesta/xv_win.manifest com_release/xv_10_b8_win_x86.ecsa
 xv com_release/arc.xv -Ndr "xv_release/_build/windows_x64"^
-    com_release/manifesta/xv_win.manifest com_release/xv_10_b7_win_x64.ecsa
+    com_release/manifesta/xv_win.manifest com_release/xv_10_b8_win_x64.ecsa
 xv com_release/arc.xv -Ndr "xv_release/_build/windows_arm64"^
-    com_release/manifesta/xv_win.manifest com_release/xv_10_b7_win_arm64.ecsa
+    com_release/manifesta/xv_win.manifest com_release/xv_10_b8_win_arm64.ecsa
 xv com_release/arc.xv -Ndr "xx_release/_build/windows_x86"^
-    com_release/manifesta/xx_win.manifest com_release/xx_10_b7_win_x86.ecsa
+    com_release/manifesta/xx_win.manifest com_release/xx_10_b8_win_x86.ecsa
 xv com_release/arc.xv -Ndr "xx_release/_build/windows_x64"^
-    com_release/manifesta/xx_win.manifest com_release/xx_10_b7_win_x64.ecsa
+    com_release/manifesta/xx_win.manifest com_release/xx_10_b8_win_x64.ecsa
 xv com_release/arc.xv -Ndr "xx_release/_build/windows_arm64"^
-    com_release/manifesta/xx_win.manifest com_release/xx_10_b7_win_arm64.ecsa
+    com_release/manifesta/xx_win.manifest com_release/xx_10_b8_win_arm64.ecsa
 CALL purify.bat
